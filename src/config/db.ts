ef876cbe17728b4dc93d21f684c87fb801315136
Hridoy-ikramul-haque <67628732+Hridoy-ikramul-haque@users.noreply.github.com>
2026-03-04 03:41:47 +0600

    modify db

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,29 +7,33 @@ const pool = new Pool({
 
 const initDB = async () => {
     await pool.query(`
-        CREATE TABLE IF NOT EXISTS abir(
+        CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(150) NOT NULL,
         email VARCHAR(150) UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
+        password TEXT NOT NULL CHECK (char_length(password)>=6),
+        phone VARCHAR(20) NOT NULL,
+        role VARCHAR(20) NOT NULL CHECK (role IN('admin','customer')),
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
         )
         `);
 
     await pool.query(`
-        CREATE TABLE IF NOT EXISTS raha(
+        CREATE TABLE IF NOT EXISTS Vehicles(
         id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES abir(id) ON DELETE CASCADE,
-        tittle VARCHAR(200) NOT NULL,
-        description TEXT,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
+        vehicle_name VARCHAR(150) NOT NULL,
+        type VARCHAR(20) NOT NULL CHECK (type IN('car','bike','van','SUV')),
+        registration_number VARCHAR(100) UNIQUE NOT NULL,
+        daily_rent_price NUMERIC NOT NULL CHECK(daily_rent_price>0),
+        availability_status VARCHAR(20) NOT NULL DEFAULT 'available' CHECK(availability_status IN('available','booked')),
+        )
+        `);
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS Bookings(
+        id SERIAL PRIMARY KEY,
+        
         )
         `)
 };
