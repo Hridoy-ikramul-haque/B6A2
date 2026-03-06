99768a1342979c3a6909ebb7987e3712eafad8b4
Hridoy-ikramul-haque <67628732+Hridoy-ikramul-haque@users.noreply.github.com>
2026-03-07 04:58:14 +0600

    vechile get post,signup created

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 import configData from ".";
 
-const pool = new Pool({
+export const pool = new Pool({
     connectionString: configData.connectionstr
 })
 
@@ -10,7 +10,7 @@ const initDB = async () => {
         CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(150) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL CHECK(email=lower(email)),
         password TEXT NOT NULL CHECK (char_length(password)>=6),
         phone VARCHAR(20) NOT NULL,
         role VARCHAR(20) NOT NULL CHECK (role IN('admin','customer')),
@@ -20,20 +20,29 @@ const initDB = async () => {
         `);
 
     await pool.query(`
-        CREATE TABLE IF NOT EXISTS Vehicles(
+        CREATE TABLE IF NOT EXISTS vehicles(
         id SERIAL PRIMARY KEY,
         vehicle_name VARCHAR(150) NOT NULL,
         type VARCHAR(20) NOT NULL CHECK (type IN('car','bike','van','SUV')),
         registration_number VARCHAR(100) UNIQUE NOT NULL,
         daily_rent_price NUMERIC NOT NULL CHECK(daily_rent_price>0),
         availability_status VARCHAR(20) NOT NULL DEFAULT 'available' CHECK(availability_status IN('available','booked')),
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
         )
         `);
 
     await pool.query(`
         CREATE TABLE IF NOT EXISTS Bookings(
         id SERIAL PRIMARY KEY,
-        
+        customer_id INT REFERENCES Users(id) ON DELETE CASCADE,
+        vehicle_id INT REFERENCES Vehicles(id) ON DELETE CASCADE,
+        rent_start_date DATE NOT NULL,
+        rent_end_date DATE NOT NULL CHECK(rent_end_date>rent_start_date),
+        total_price NUMERIC NOT NULL CHECK(total_price>0),
+        status VARCHAR(20) NOT NULL DEFAULT 'active' CHECK(status IN('active','cancelled','returned')),
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
         )
         `)
 };
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,101 @@
 import express, { Request, Response } from "express";
 import initDB from "./config/db";
 import configData from "./config";
+import { pool } from "./config/db";
 
 
-const app = express(); 
-
-const port = configData.port||6000;
+const app = express();
+app.use(express.json());
+const port = configData.port || 6000;
 
 initDB();
 console.log(process.cwd());
 
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
         success: true,
         message: 'basic test'
     })
 });
+
+// Create User
+app.post('/api/v1/auth/signup', async (req: Request, res: Response) => {
+    try {
+        const { name, email, password, phone, role } = req.body;
+        console.log(email);
+        const result = await pool.query(`INSERT INTO users(name,email,password,phone,role) VALUES($1,$2,$3,$4,$5) RETURNING *`, [name, email, password, phone, role]);
+        console.log(result);
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            data: result.rows
+        })
+    } catch (err: any) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "Login Unsuccessful"
+        })
+    }
+})
+
+
+// Create Vehicle 
+app.post('/api/v1/vehicles', async (req: Request, res: Response) => {
+    try {
+        const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = req.body;
+        const result = await pool.query(`INSERT INTO vehicles(vehicle_name, type, registration_number, daily_rent_price, availability_status) VALUES($1,$2,$3,$4,$5) RETURNING *`, [vehicle_name, type, registration_number, daily_rent_price, availability_status])
+        res.status(201).json({
+            success: true,
+            message: "Vehicle created successfully",
+            data: result.rows
+        })
+    }
+    catch (err: any) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "Unsuccessful"
+        })
+    }
+})
+
+
+// View all vehicle 
+app.get('/api/v1/vehicles', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM vehicles`);
+        if (result.rows.length == 0) {
+            res.status(200).json({
+                success: true,
+                message: "No vehicles found",
+                data: result.rows
+            })
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                message: "Vehicle retrieved successfully",
+                data: result.rows
+            })
+        }
+
+    } catch (error: any) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            error: error
+        })
+    }
+})
+
+app.get('/api/v1/vehicles/:vehicleId', (req: Request, res: Response) => {
+    try {
+
+    } catch (err) {
+
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
